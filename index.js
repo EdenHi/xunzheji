509b70608960093367e83a552ffaddfe3c7cef0a
陈钱 <1041001884@qq.com>
2021-07-27 18:18:05 +0800

    11

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/pages/Shop/dingzhi';
+import App from './App';
 import {name as appName} from './app.json';
 import textInput from './src/components/textInput';
 import {Login, Register} from "./src/pages/index"
