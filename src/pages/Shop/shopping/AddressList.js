57038582e1304a250d3424bd5970652649e1f6ec
ppx <572504596@qq.com>
2021-07-29 10:38:19 +0800

    123

--- src/pages/Shop/shopping/AddressList.js
+++ src/pages/Shop/shopping/AddressList.js
@@ -1,30 +1,98 @@
-import React, { Component } from 'react'
-import { View,Image,Text,TouchableOpacity, Dimensions,TextInput } from 'react-native'
-
-const {width,height} = Dimensions.get("window")
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import { View,Image,Text,TouchableOpacity, Dimensions,TextInput, FlatList,AsyncStorage,DeviceEventEmitter } from 'react-native';
+const {width,height} = Dimensions.get('window');
 
 export default class AddressList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data:[],
+            username:'',
+            isLoding:false,
+        };
+    }
+
+    get_list(){
+        AsyncStorage.getItem('username',(error,result)=>{
+            if (!error) {
+                this.setState({
+                    username:result,
+                });
+                console.log('username',result);
+                fetch('http://192.168.50.117:3000/shop/selectdizhi', {
+                    method: 'POST',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        username:result,
+                    }),
+                    }) .then((response) => response.json())
+                    .then((responseJson) => {
+                        console.log(responseJson);
+                        this.setState({
+                            data:responseJson,
+                        });
+                    })
+            } else {
+                console.log('获取数据失败',error);
+            }
+        });
+    }
+    loding(){
+        this.setState({
+            isLoding : true,
+        });
+
+        setTimeout(() => {
+            this.setState({
+                isLoding : false,
+            });
+            this.get_list();
+        }, 1000);
+    }
+    componentDidMount(){
+        this.get_list();
+        this.listener = DeviceEventEmitter.addListener('test',this.loding.bind(this))
+    }
+    componentWillUnmount(){
+        this.listener.remove();
+        }
+    renderItem({item,index}){
+        return (
+            <View key = {index} style={{flexDirection:'row',height:60,justifyContent:'space-between',alignItems:'center'}}>
+                    <View>
+                    <View style={{flexDirection:'row',height:30 }}><Text style={{fontSize:16,width:width * 0.2,fontWeight:'bold'}}>{item.name}</Text><Text>{item.phone}</Text></View>
+                    <View style={{flexDirection:'row',height:30 }}><Text style={{marginRight:10}}>{item.dizhi}</Text><Text style={{marginRight:10}}>{item.xiangxi}</Text></View>
+                    </View>
+                    <View  style={{width:width * 0.03,height:width * 0.03,borderWidth:1}}/>
+                </View>
+        );
+    }
     render() {
+        const {data} = this.state;
         return (
             <View style={{padding:10}}>
-                <View style={{height:height*0.85}}>
-                <View style={{flexDirection:"row",alignItems:"center",marginBottom:10,marginTop:10,borderBottomWidth:0.5,borderBottomColor:"#808080",justifyContent:"space-between",height:50}}>
-                    <View  style={{width:width*0.03,height:width*0.03,borderWidth:1}}/>
-                    <Text style={{width:width*0.8,textAlign:"center",fontSize:18,fontWeight:"bold"}}>
+                <View style={{height:height * 0.85}}>
+                <View style={{flexDirection:'row',alignItems:'center',marginBottom:10,marginTop:10,borderBottomWidth:0.5,borderBottomColor:'#808080',justifyContent:'space-between',height:50}}>
+                    <View  style={{width:width * 0.03,height:width * 0.03,borderWidth:1}}/>
+                    <Text style={{width:width * 0.8,textAlign:'center',fontSize:18,fontWeight:'bold'}}>
                         地址列表
                     </Text>
-                    <View style={{width:width*0.03,height:height*0.03}}></View>
+                    <View style={{width:width * 0.03,height:height * 0.03}} />
                 </View>
-                <View style={{flexDirection:"row",height:60,justifyContent:"space-between",alignItems:"center"}}>
-                    <View>
-                    <View style={{flexDirection:"row",height:30 }}><Text style={{fontSize:16,width:width*0.2,fontWeight:"bold"}}>收货人</Text><Text>10086</Text></View>
-                    <View style={{flexDirection:"row",height:30 }}><Text style={{marginRight:10}}>浙江省</Text><Text style={{marginRight:10}}>杭州市</Text><Text style={{marginRight:10}}>拱墅区</Text><Text style={{marginRight:10}}>浙江树人大学</Text></View>
-                    </View>
-                    <View  style={{width:width*0.03,height:width*0.03,borderWidth:1}}/>
+                <FlatList
+                refreshing = {this.state.isLoding} //设置是否在刷新
+                onRefresh = {this.loding.bind(this)} //下拉刷新结束
+                keyExtractor={(item, index) => (index + '1')}
+                data = {data}
+                renderItem = {this.renderItem.bind(this)}/>
                 </View>
-                </View>
-                <TouchableOpacity onPress={()=>{this.props.navigation.navigate('Address')}} underlayColor="red"><View style={{backgroundColor:`#ff0000`,height:40,borderRadius:20,justifyContent:"center"}}><Text style={{textAlign:"center",fontSize:18,color:"#fff"}}>添加新地址</Text></View></TouchableOpacity>
+                <TouchableOpacity onPress={()=>{this.props.navigation.navigate('Address');}} underlayColor="red"><View style={{backgroundColor:'#ff0000',height:40,borderRadius:20,justifyContent:'center'}}><Text style={{textAlign:'center',fontSize:18,color:'#fff'}}>添加新地址</Text></View></TouchableOpacity>
             </View>
-        )
+        );
     }
 }
