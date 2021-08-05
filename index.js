aaf46dc48fce6f77847541e0584cb181d2696de8
EdenZhang <2824377522@qq.com>
2021-08-05 08:36:11 +0800

    AR

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 // import App from './src/pages/Forum/UserDetails/people';
 import {name as appName} from './app.json';
-console.disableYellowBox = true
+LogBox.ignoreAllLogs(true);
 AppRegistry.registerComponent(appName, () => App);
