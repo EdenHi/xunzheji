e357c2f47d894e8035321fbad6f54f320ef16260
EdenZhang <2824377522@qq.com>
2021-08-14 14:41:42 +0800

    chat

--- src/nav/StackNav.js
+++ src/nav/StackNav.js
@@ -66,14 +66,11 @@ import {
   Dingdan,
   Shoplist,
   Shopdetails,
-<<<<<<< HEAD
-  Story,
-  Story2,
-=======
   zhifu_cart,
   JiFen,
   duihuan_jinbi,
->>>>>>> 2f364fb8a31e1078cdec7e846fdec022474c318e
+  Story,
+  Story2,
 } from '../pages/index';
 import Ranking from '../Ranking';
 import { startClock } from 'react-native-reanimated';
@@ -145,8 +142,6 @@ export default function StackNav() {
       <Stack.Screen name="Video" component={Video} />
       <Stack.Screen name="Zhan" component={Zhan} />
       <Stack.Screen name="JieShao" component={JieShao} />
-
-
       <Stack.Screen name="tag" component={tag} />
       <Stack.Screen name="Shopclassifymore" component={Shopclassifymore} />
       <Stack.Screen name="Swop" component={Swop} />
@@ -156,16 +151,13 @@ export default function StackNav() {
       <Stack.Screen name="Dingdan" component={Dingdan} />
       <Stack.Screen name="Shoplist" component={Shoplist} />
       <Stack.Screen name="Shopdetails" component={Shopdetails} />
-<<<<<<< HEAD
 
       <Stack.Screen name="Story" component={Story} />      
       <Stack.Screen name="Story2" component={Story2} />      
 
-=======
       <Stack.Screen name="zhifu_cart" component={zhifu_cart} />
       <Stack.Screen name="JiFen" component={JiFen} />
       <Stack.Screen name="duihuan_jinbi" component={duihuan_jinbi} />
->>>>>>> 2f364fb8a31e1078cdec7e846fdec022474c318e
     </Stack.Navigator>
   );
 }
