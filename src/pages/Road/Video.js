4c7bb9a58fff28f95f78f24841a7e45911eb96d7
EdenZhang <2824377522@qq.com>
2021-08-18 13:49:07 +0800

    8780

--- src/pages/Road/Video.js
+++ src/pages/Road/Video.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { View, Dimensions, Text, TouchableOpacity, ImageBackground, Image, Modal, ScrollView,ToastAndroid, } from 'react-native';
+import { View, Dimensions, Text, TouchableOpacity, ImageBackground, Image, Modal, ScrollView, ToastAndroid, } from 'react-native';
 import Video from 'react-native-video';
 const { width, height } = Dimensions.get('window');
 import LinearGradient from 'react-native-linear-gradient'
@@ -10,6 +10,7 @@ import { Animated } from 'react-native';
 import axios from 'axios';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
 export default class componentName extends Component {
     constructor(props) {
         super(props)
@@ -23,10 +24,10 @@ export default class componentName extends Component {
             step: 0,
             road: 0,
             prevSteps: 0,
-            gold:0,
-            mission1:false,
-            mission2:false,
-            mission3:false,
+            gold: 0,
+            mission1: false,
+            mission2: false,
+            mission3: false,
         }
     }
 
@@ -107,25 +108,61 @@ export default class componentName extends Component {
         })
     }
     //更新金币数
-    getAllGold(gold1){
+    getAllGold(gold1) {
         console.log(gold1);
-        var oldgold =this.state.gold;
-        oldgold+=gold1;
-        this.setState({gold:oldgold})
+        var oldgold = this.state.gold;
+        oldgold += gold1;
+        this.setState({ gold: oldgold })
     }
     /* 更新任务完成情况 */
-    isFinished(mission){
-        if(mission=='mission1'){
-            this.setState({mission1:true})
-        }else if(mission=='mission2'){
-            this.setState({mission2:true})
-        }else if(mission=='mission3'){
-            this.setState({mission3:true})
+    isFinished(mission) {
+        if (mission == 'mission1') {
+            this.setState({ mission1: true })
+        } else if (mission == 'mission2') {
+            this.setState({ mission2: true })
+        } else if (mission == 'mission3') {
+            this.setState({ mission3: true })
         }
         console.log(this.state.mission1);
         console.log(this.state.mission2);
         console.log(this.state.mission3);
     }
+    onPhotosFetchedSuccess(data) {
+        const photos = data.edges.map((asset) => {
+          return asset.node.image;
+        });
+        console.log(photos);
+        /**
+        On Android, this should log something like:
+        [
+          {
+            "uri": "file:/storage/emulated/0/DCIM/Camera/IMG_20160120_172426830.jpg",
+            "width":3006,
+            "height":5344,
+            "orientation": 90
+          },
+          {
+            "uri": "file:/storage/emulated/0/DCIM/Camera/IMG_20160116_153526816_TOP.jpg",
+            "width": 5344,
+            "height": 3006,
+            "orientation": 0
+          }
+          ...
+        ]
+        **/
+      }
+      onPhotosFetchError(err) {
+        // Handle error here
+      }
+      
+      fetchPhotos(count = 10, after) {
+        CameraRoll.getPhotos({
+          // take the first n photos after given photo uri
+          first: count,
+          // after
+          after: "file:/storage/emulated/0/DCIM/Camera/IMG_20151126_115520477.jpg",
+        }, this.onPhotosFetchedSuccess.bind(this), this.onPhotosFetchError.bind(this));
+      }
     render() {
         const { modalVisible, modalVisible2 } = this.state;
         return (
@@ -318,8 +355,8 @@ export default class componentName extends Component {
                             </View>
                             <View style={{ flex: 0.9, backgroundColor: '#7cc0c0', elevation: 5 }}>
                                 <View style={{ height: height * 0.1, width, flexDirection: 'row', backgroundColor: 'rgba(255,255,255,0.25)', marginVertical: '0.25%' }}>
-                                    <Text style={{ marginLeft: '4%', width: '20%', height: '100%', borderWidth: 0, textAlignVertical: 'center', fontSize: 15, fontWeight: 'bold',color:'#333' }}>签到有奖:</Text>
-                                    <Text style={{ width: '40%', height: '100%', borderWidth: 0, textAlignVertical: 'center', fontSize: 15, fontWeight: 'bold',color:'#333' }}>每日签到可获得 5 <FontAwesome5
+                                    <Text style={{ marginLeft: '4%', width: '20%', height: '100%', borderWidth: 0, textAlignVertical: 'center', fontSize: 15, fontWeight: 'bold', color: '#333' }}>签到有奖:</Text>
+                                    <Text style={{ width: '40%', height: '100%', borderWidth: 0, textAlignVertical: 'center', fontSize: 15, fontWeight: 'bold', color: '#333' }}>每日签到可获得 5 <FontAwesome5
                                         name='coins'
                                         size={15}
                                         color='gold'
@@ -330,9 +367,9 @@ export default class componentName extends Component {
                                     </TouchableOpacity>
                                 </View>
 
-                                <ThisMission target="孝顺镇" progress="1" gold={5} sendGold={this.getAllGold.bind(this)} step={this.state.step} isGet={this.state.mission1} isFinished={this.isFinished.bind(this)}/>
-                                <ThisMission target="龙游" progress="2"  gold={10} sendGold={this.getAllGold.bind(this)} step={this.state.step} isGet={this.state.mission2} isFinished={this.isFinished.bind(this)}/>
-                                <ThisMission target="上饶" progress="3"  gold={10} sendGold={this.getAllGold.bind(this)} step={this.state.step} isGet={this.state.mission3} isFinished={this.isFinished.bind(this)}/>
+                                <ThisMission target="孝顺镇" progress="1" gold={5} sendGold={this.getAllGold.bind(this)} step={this.state.step} isGet={this.state.mission1} isFinished={this.isFinished.bind(this)} />
+                                <ThisMission target="龙游" progress="2" gold={10} sendGold={this.getAllGold.bind(this)} step={this.state.step} isGet={this.state.mission2} isFinished={this.isFinished.bind(this)} />
+                                <ThisMission target="上饶" progress="3" gold={10} sendGold={this.getAllGold.bind(this)} step={this.state.step} isGet={this.state.mission3} isFinished={this.isFinished.bind(this)} />
 
 
 
@@ -351,7 +388,9 @@ export default class componentName extends Component {
                     </View>
                     <Image style={{ height: height * 0.8, width: width }} source={{ uri: 'http://8.142.11.85:3000/public/images/nanlu.jpg' }}></Image>
                     <View style={{ zIndex: 10, backgroundColor: 'rgba(255,255,255,255,0.5)', width: '15%', height: width * 0.15, marginTop: -height * 0.7, marginBottom: height * 0.65, marginLeft: width * 0.875, borderRadius: 10, }}>
-                        <TouchableOpacity style={{}} onPress={() => this.props.navigation.navigate('AR')}>
+                        <TouchableOpacity style={{}} onPress={() => this.refs.viewShot.capture().then(uri => {
+                            console.log(uri);
+                        })}>
                             {/* <Text style={{ fontSize: 20, fontWeight: 'bold', width: '100%', textAlign: 'center', height: '100%', textAlignVertical: 'center' }}>Ar</Text> */}
                             <MaterialCommunityIcons
                                 name="augmented-reality"
@@ -360,14 +399,19 @@ export default class componentName extends Component {
                             />
                         </TouchableOpacity>
                     </View>
-                    <Video
-                        source={{ uri: 'http://8.142.11.85:8080/1.mp4' }}
-                        paused={this.state.poused}
-                        resizeMode="stretch"
-                        posterResizeMode='contain'
-                        style={{ zIndex: 0, height: height * 0.8, width: width, marginTop: -height * 0.8 }}
-                        onProgress={this.onProgress}
-                    />
+                    <ViewShot ref="viewShot" options={{ format: 'jpg', quality: 1 }}>
+                        <View>
+                            <Video
+                                source={{ uri: 'http://8.142.11.85:8080/1.mp4' }}
+                                paused={this.state.poused}
+                                resizeMode="stretch"
+                                posterResizeMode='contain'
+                                style={{ zIndex: 0, height: height * 0.8, width: width, marginTop: -height * 0.8 }}
+                                onProgress={this.onProgress}
+                            />
+                        </View>
+                    </ViewShot>
+
                     {/* 底部 */}
                     <View style={{ height: height * 0.2, flexDirection: 'row', justifyContent: 'space-around', marginTop: -height * 0.075 }}>
                         <TouchableOpacity activeOpacity={1} onPress={() => this.setModalVisible(!modalVisible)} style={{ height: '100%', width: '30%', justifyContent: 'space-around' }}>
@@ -376,56 +420,56 @@ export default class componentName extends Component {
                         <TouchableOpacity activeOpacity={1} onPress={() => { this.Start(), this.continuePlay(), this.updateStep(), console.log(this.state.play); }} style={{ width: width * 0.25, height: width * 0.25, borderRadius: 60, backgroundColor: '#7cc0c0', alignSelf: 'center', marginTop: -height * 0.07, borderWidth: 5, borderColor: '#fff' }}>
                             <Text style={{ textAlign: 'center', textAlignVertical: 'center', height: '100%', fontSize: 30, fontWeight: 'bold', color: '#fff' }}>Go</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={() => {this.setModalVisible2(!modalVisible2)}} style={{ height: '100%', width: '30%', justifyContent: 'space-around', alignItems: 'flex-end', borderWidth: 0 }}>
+                        <TouchableOpacity onPress={() => { this.setModalVisible2(!modalVisible2) }} style={{ height: '100%', width: '30%', justifyContent: 'space-around', alignItems: 'flex-end', borderWidth: 0 }}>
                             <Image style={{ borderWidth: 1, width: width * 0.15, height: width * 0.15, marginTop: 0, alignSelf: 'center' }} source={{ uri: 'http://8.142.11.85:3000/public/images/16-travel.png' }}></Image>
                         </TouchableOpacity>
                     </View>
                 </LinearGradient>
-            </View>
+            </View >
         );
     }
 }
 
 
 class ThisMission extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            isGet:this.props.isGet,
-            goldNum:0
+        this.state = {
+            isGet: this.props.isGet,
+            goldNum: 0
         }
-    }        
-    getGold(){
-            if(this.state.isGet==false&&this.props.step>((this.props.progress*200)/3)){
-                this.setState({isGet:true})
-                this.sendGold()
-                this.sendMission()
-                ToastAndroid.showWithGravity('领取成功',1000,ToastAndroid.BOTTOM)
-            }else if(this.state.isGet==true){
-                ToastAndroid.showWithGravity('请不要重复领取',1000,ToastAndroid.BOTTOM) 
-            }else{
-                ToastAndroid.showWithGravity('任务未完成，加油',500,ToastAndroid.BOTTOM) 
-            }
+    }
+    getGold() {
+        if (this.state.isGet == false && this.props.step > ((this.props.progress * 200) / 3)) {
+            this.setState({ isGet: true })
+            this.sendGold()
+            this.sendMission()
+            ToastAndroid.showWithGravity('领取成功', 1000, ToastAndroid.BOTTOM)
+        } else if (this.state.isGet == true) {
+            ToastAndroid.showWithGravity('请不要重复领取', 1000, ToastAndroid.BOTTOM)
+        } else {
+            ToastAndroid.showWithGravity('任务未完成，加油', 500, ToastAndroid.BOTTOM)
+        }
 
 
     }
-    sendGold(){
+    sendGold() {
         this.props.sendGold(this.props.gold)
     }
-    sendMission(){
-        this.props.isFinished("mission"+this.props.progress)
-        console.log("mission"+this.props.progress);
+    sendMission() {
+        this.props.isFinished("mission" + this.props.progress)
+        console.log("mission" + this.props.progress);
     }
 
     render() {
-        let Lingqu=this.state.isGet? <Text style={{ height: '100%', width: '100%', textAlign: 'center', textAlignVertical: 'center', fontsize: 15, fontWeight: 'bold', color: '#fff' ,}}>已领取</Text>:<Text style={{ height: '100%', width: '100%', textAlign: 'center', textAlignVertical: 'center', fontsize: 15, fontWeight: 'bold', color: '#fff' ,backgroundColor:'orange',borderRadius:20}}>领取</Text>
+        let Lingqu = this.state.isGet ? <Text style={{ height: '100%', width: '100%', textAlign: 'center', textAlignVertical: 'center', fontsize: 15, fontWeight: 'bold', color: '#fff', }}>已领取</Text> : <Text style={{ height: '100%', width: '100%', textAlign: 'center', textAlignVertical: 'center', fontsize: 15, fontWeight: 'bold', color: '#fff', backgroundColor: 'orange', borderRadius: 20 }}>领取</Text>
 
         return (
             <View style={{ height: height * 0.1, width, flexDirection: 'row', backgroundColor: 'rgba(255,255,255,0.25)', marginVertical: '0.25%' }}>
                 <View style={{ flexDirection: 'column', justifyContent: 'space-around' }}>
                     <View style={{ flexDirection: 'row', width: '70%', borderWidth: 0 }}>
-                        <Text style={{ marginLeft: '7.5%', width: 0.2 * width, height: '100%', borderWidth: 0, textAlignVertical: 'center', fontSize: 15, fontWeight: 'bold',color:'#333333' }}>任务目标:</Text>
-                        <Text style={{ width: 0.5 * width, height: '100%', borderWidth: 0, textAlignVertical: 'center', fontSize: 15, fontWeight: 'bold' ,color:'#333'}}>{this.props.target}( {this.props.gold} <FontAwesome5
+                        <Text style={{ marginLeft: '7.5%', width: 0.2 * width, height: '100%', borderWidth: 0, textAlignVertical: 'center', fontSize: 15, fontWeight: 'bold', color: '#333333' }}>任务目标:</Text>
+                        <Text style={{ width: 0.5 * width, height: '100%', borderWidth: 0, textAlignVertical: 'center', fontSize: 15, fontWeight: 'bold', color: '#333' }}>{this.props.target}( {this.props.gold} <FontAwesome5
                             name='coins'
                             size={15}
                             color='gold'
@@ -435,7 +479,7 @@ class ThisMission extends Component {
                         <Text style={{ marginLeft: '5%', borderWidth: 0, color: '#333333' }}>大约走过{this.props.progress}00公里，进度为{this.props.progress}/3</Text>
                     </View>
                 </View>
-                <TouchableOpacity onPress={()=>{this.getGold()}} activeOpacity={1} style={{ borderWidth: 0, width: width * 0.2, height: '50%', backgroundColor: 'rgb(249,200,159)', marginVertical: '6%', borderRadius: 20, marginHorizontal: '0%', }}>
+                <TouchableOpacity onPress={() => { this.getGold() }} activeOpacity={1} style={{ borderWidth: 0, width: width * 0.2, height: '50%', backgroundColor: 'rgb(249,200,159)', marginVertical: '6%', borderRadius: 20, marginHorizontal: '0%', }}>
                     {Lingqu}
                 </TouchableOpacity>
             </View>
