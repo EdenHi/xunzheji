137386818813c45a52309033172a038d2ce57ea2
EdenZhang <2824377522@qq.com>
2021-08-19 19:49:55 +0800

    666

--- index.js
+++ index.js
@@ -5,30 +5,8 @@
 
 import {AppRegistry, LogBox} from 'react-native';
 // import App from './src/pages/load/index';
- import App from './src/components/danmu/Barrage';
+ import App from './App';
 import {name as appName} from './app.json';
 LogBox.ignoreAllLogs(true);
 
 AppRegistry.registerComponent(appName, () => App);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
