9b126cbb9fcfa7368906691424c0088c6ecaacb8
Git1 <2482394937@qq.com>
2021-08-19 15:59:06 +0800

    11

--- src/components/danmu/Barrage.js
+++ src/components/danmu/Barrage.js
@@ -74,43 +74,17 @@ export default class luntan extends Component {
         const { navigation } = this.props;
         console.debug('Barrage');
         return (
-            <View style={styles.container}>
-                <View style={styles.header} >
-                    <TouchableOpacity
-                        style={styles.back}
-                        onPress={() => navigation.goBack()}
-                    >
-                        <Image style={{ width: "100%", height: "100%" }}
-                            source={{uri:'http://picture.ik123.com/uploads/allimg/201118/12-20111Q64451.jpg'}}></Image>
-                    </TouchableOpacity>
-                    <Text style={{ fontSize: 15, }}>论坛</Text>
-                    <TouchableOpacity
-                        style={styles.back}
-                    >
-                        <Image style={{ width: "100%", height: "100%" }}
-                           source={{uri:'http://picture.ik123.com/uploads/allimg/201118/12-20111Q64451.jpg'}}></Image>
-                    </TouchableOpacity>
-                </View>
+            <View style={styles.container}>           
                 <View style={styles.barrageView}>
-
-                    <BarrageMoveView newMessages={this.state.data} numberOfLines={5} speed={0.8} />
-
-
+                    <BarrageMoveView style={{}} newMessages={this.state.data} numberOfLines={5} speed={0.8} />
                 </View>
                 <View style={{ width: "100%", height: "27%", alignItems: "center", justifyContent: "center", backgroundColor: "red", borderTopLeftRadius: 15, borderTopRightRadius: 15 }}>
-                    <View style={{ width: "100%", height: "70%", backgroundColor: "#FF7A7A", flexDirection: "row", alignItems: "center" }}>
-                        <View style={{ width: "60%", height: "90%", marginLeft: 20, marginTop: 40 }}>
-                            <Text style={{ fontSize: 15 }}>近日，国家发展改革委发布了《关于“十四五”时期深化价格机制改革行动方案的通知》（简称《行动方案》）</Text>
-                        </View>
-                        <Image style={{ width: 100, height: 100, marginLeft: 15 }} source={{uri:'http://picture.ik123.com/uploads/allimg/201118/12-20111Q64451.jpg'}}></Image>
-                    </View>
                     <View style={{ width: "100%", height: "30%", backgroundColor: "#FF7A7A", flexDirection: "row", alignItems: "center" }}>
                         <TextInput  style={{ width: "70%", height: 40, backgroundColor: "#E0E0E0", borderRadius: 30, marginLeft: 10 }}
                         onChangeText={txt=>{
                             this.setState({txt});
                         }}
                         ref={input => { this.textInput = input }} 
-                        
                         ></TextInput>
                         <TouchableOpacity onPress={()=>{this.data.push(this.state.txt);Keyboard.dismiss();this.textInput.clear()}} style={{ width: 80, height: 40, backgroundColor: "#FF3D3D", borderRadius: 50, marginLeft: 15, alignItems: "center", justifyContent: "center" }}>
                             <Text style={{ fontSize: 15 }}>发送</Text>
@@ -130,6 +104,7 @@ const styles = StyleSheet.create({
     barrageView: {
         width: '100%',
         height: "65%",
+        
     },
     header: {
         width: "100%",
--- src/pages/Shop/CulturalCreation.js
+++ src/pages/Shop/CulturalCreation.js
@@ -1,65 +1,121 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView,FlatList, TouchableOpacity} from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import Paper from './paper/paper'
+import React, {
+  Component,
+} from 'react';
+import {
+  StyleSheet,
+  Image,
+  PanResponder,
+  Animated,
+  Dimensions,
+  TouchableOpacity,
+  Text
+} from 'react-native';
+import { View } from 'react-native-animatable';
+import AntDesign from "react-native-vector-icons/AntDesign"
+import LinearGradient from 'react-native-linear-gradient'
+const {width, height} = Dimensions.get('window');
 
-export default function CulturalCreation ({navigation}){
-  return (
-    <View style={styles.container}>
-      <View style={{
-          backgroundColor: "#fff",
-          height: 45,
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: 'row',
-        }}>
-          <TouchableOpacity
-            activeOpacity={0.8}
-            onPress={() => navigation.goBack()}
-            style={{
-              width: 50,
-              position: "absolute",
-              height: "100%",
-              justifyContent: "center",
-              alignItems: "center",
-              left: 0
-            }}>
-            <FontAwesome name={'angle-left'} size={25} color={'#000'} />
-          </TouchableOpacity>
-          <View style={{  }}>
-            <Text style={{
-              fontSize: 18,
-              textAlign: 'center',
-              textAlignVertical: 'center',
-            }}>包你满意</Text>
+class Draggable extends Component{
+  constructor(props){
+      super(props);
+      this.state = {
+          pan: new Animated.ValueXY(),
+          scale: new Animated.Value(1),
+          rotate: new Animated.Value(0)
+      };
+  }
+  componentWillMount() {
+      this._panResponder = PanResponder.create({
+          onMoveShouldSetResponderCapture: () => true,
+          onMoveShouldSetPanResponderCapture: () => true,
+          // 设置初始位置
+          onPanResponderGrant: (e, gestureState) => {
+              this.state.pan.setOffset({
+                  x: this.state.pan.x._value,
+                  y: this.state.pan.y._value
+              });
+              this.state.pan.setValue({x: 0, y: 0});
+              Animated.spring(this.state.scale, {
+                  toValue: 1.3,
+                  friction: 3 }
+              ).start();
+              Animated.timing(this.state.rotate, {
+                  toValue: 25,
+                  duration: 300
+              }).start();
+          },
+          // 使用拖拽的偏移量来定位
+          onPanResponderMove: Animated.event([
+              null, {dx: this.state.pan.x, dy: this.state.pan.y},
+          ]),
+          onPanResponderRelease: (e, {vx, vy}) => {
+              this.state.pan.flattenOffset();
+              // Animated.spring(
+              //     this.state.pan,
+              //     {toValue: {x: 0, y: 0}}
+              // ).start();
+              Animated.spring(
+                  this.state.scale,
+                  { toValue: 1, friction: 3 }
+              ).start();
+              Animated.timing(this.state.rotate, {
+                  toValue: 0,
+                  duration: 300
+              }).start();
+          }
+      });
+  }
+  render(){
+      // 从state中取出pan
+      
+      const { pan, scale } = this.state;
+      // 从pan里计算出偏移量
+      const [translateX, translateY] = [pan.x, pan.y];
+      // 计算旋转
+      const rotate = this.state.rotate.interpolate({
+          inputRange: [0, 100],
+          outputRange: ['0deg', '00deg']
+      });
+      // 设置transform为偏移量
+      const imageStyle = {transform: [{translateX}, {translateY},  {scale}, {rotate}]};
+      return (
+        <View style={{width:width,height:height,alignItems:"center"}}>
+          <View style={{ flexDirection: "row",backgroundColor:"#fff",width:width, alignItems: "center", height: height * 0.07, justifyContent: "center" }}>
+                <TouchableOpacity
+                 activeOpacity={1} style={{}}>
+                    <AntDesign onPress={() => this.props.navigation.goBack()} style={{ textAlignVertical: 'center', height: "100%", color: "#333333" }} name="left" size={20} color="#000000" />
+                </TouchableOpacity>
+                <Text style={{ fontSize: 15, fontWeight: "bold", color: "#333333", width: width * 0.85, marginLeft: "2%" }}>定制</Text>
+            </View>
+            <View style={{width:width,height:height*0.65,backgroundColor:"#fff"}}>
+
+          <Animated.View style={[styles.container,imageStyle]} {...this._panResponder.panHandlers}>
+              <Image style={{width:50,height:50}} source={require('../img/T.jpg')}/>
+          </Animated.View>
+          </View>
+          <View style={{width:width,height:height*0.35,backgroundColor:"#fff",borderTopRightRadius:20,elevation:20,position:"absolute",bottom:0,borderTopLeftRadius:20}}>
+          <LinearGradient style={{ width: width, height: "100%" ,alignItems:"center",borderTopRightRadius:20,borderTopLeftRadius:20,elevation:20,}} colors={["#7cc0bf", "#fff"]} >
+            <View style={{width:"100%",height:"50%",flexDirection:"row",justifyContent:"space-around",alignItems:"center"}}>
+            <TouchableOpacity style={{width:width*0.25,height:width*0.25}}><Image style={{width:"100%",height:"100%"}} source={require("../img/T.jpg")}></Image></TouchableOpacity>
+            <TouchableOpacity style={{width:width*0.25,height:width*0.25}}><Image style={{width:"100%",height:"100%"}} source={require("../img/T.jpg")}></Image></TouchableOpacity>
+            <TouchableOpacity style={{width:width*0.25,height:width*0.25}}><Image style={{width:"100%",height:"100%"}} source={require("../img/T.jpg")}></Image></TouchableOpacity>
+            </View>
+            <View style={{width:"100%",height:"50%",flexDirection:"row",justifyContent:"space-around",alignItems:"center"}}>
+            <TouchableOpacity style={{width:width*0.25,height:width*0.25}}><Image style={{width:"100%",height:"100%"}} source={require("../img/T.jpg")}></Image></TouchableOpacity>
+            <TouchableOpacity style={{width:width*0.25,height:width*0.25}}><Image style={{width:"100%",height:"100%"}} source={require("../img/T.jpg")}></Image></TouchableOpacity>
+            <TouchableOpacity style={{width:width*0.25,height:width*0.25}}><Image style={{width:"100%",height:"100%"}} source={require("../img/T.jpg")}></Image></TouchableOpacity>
+            </View>
+            </LinearGradient>
           </View>
         </View>
-      <View style={{width:"100%",height:"100%"}}>
-     <Paper></Paper>
-      </View>
-    </View>
-
-    
-  );
+      )
+  }
 }
-
-
-
+export default Draggable;
 const styles = StyleSheet.create({
-    container: {
-       flex: 1
-    },
-    letter_up_1:{
-      width:'80%',
-      backgroundColor:'#fff',
-      top:10,
-      left:25
-    },
-    word:{
-      fontSize:14,
-      color:'#000',
-      letterSpacing:1,
-      top:4,
-      left:4
-    }
-  }); 
+  container: {
+      position: 'absolute',
+      left: 100,
+      top: 200,
+  }
+});
