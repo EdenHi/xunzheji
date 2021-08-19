2a3b13488aef2b14796f1cea42ba36ae62acd51f
Git1 <2482394937@qq.com>
2021-08-19 19:58:43 +0800

    弹幕

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
  */
 
 import {AppRegistry, LogBox} from 'react-native';
-// import App from './src/pages/load/index';
- import App from './src/components/danmu/Barrage';
+import App from './App';
+//  import App from './src/components/danmu/Barrage';
 import {name as appName} from './app.json';
 LogBox.ignoreAllLogs(true);
 
