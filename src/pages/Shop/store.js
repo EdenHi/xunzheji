db0e89dfd8455b5e1a1df4027d90d0ebe5690d7b
Git1 <2482394937@qq.com>
2021-08-22 10:20:16 +0800

    1

--- src/pages/Shop/store.js
+++ src/pages/Shop/store.js
@@ -731,9 +731,7 @@ const styles = StyleSheet.create({
     marginTop: "2%",
    marginRight:35,
     borderRadius: 15,
-    // elevation:10,
 
-    // backgroundColor:"red",
    
   },
   waterfall: {
