3d44e6bedf23bcb39f5ebd03cb6e97eb936c4584
Git1 <2482394937@qq.com>
2021-08-29 19:26:11 +0800

    1

--- src/pages/Shop/Offline/go_map.js
+++ src/pages/Shop/Offline/go_map.js
@@ -6,10 +6,11 @@ import WebView from 'react-native-webview';
 
 export default class go_map extends Component {
     render() {
+        let a ='116.478346'
         return (
             <View style={{flex:1}}>
-                <WebView source={{uri:'https://uri.amap.com/navigation?from=116.478346,39.997361,startpoint&to=116.3246,39.966577,endpoint&via=116.402796,39.936915,midwaypoint&mode=car&policy=1&src=mypage&coordinate=gaode&callnative=0'}}
-                style={{height:100,width:100}}/>
+                <WebView source={{uri:`https://uri.amap.com/navigation?from="${a}",39.997361,startpoint&to=116.3246,39.966577,endpoint&via=116.402796,39.936915,midwaypoint&mode=car&policy=1&src=mypage&coordinate=gaode&callnative=0`}}
+                style={{height:'100%',width:'100%'}}/>
             </View>
         );
     }
