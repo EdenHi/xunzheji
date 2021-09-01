a66472a00f01f0da4180743117747528cb44a5e3
陈钱 <1041001884@qq.com>
2021-09-01 16:44:22 +0800

    123

--- src/pages/HomeScreen/HOME/ZhenCe2.js
+++ src/pages/HomeScreen/HOME/ZhenCe2.js
@@ -9,7 +9,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const { width, height } = Dimensions.get("window")
 
-export default class ZhenCe extends Component {
+export default class ZhenCe2 extends Component {
     render() {
         return (
             <View style={{ flex: 1 }}>
@@ -25,14 +25,14 @@ export default class ZhenCe extends Component {
                 <ScrollView>
                     <View style={{ width, marginHorizontal: width * 0.05 }}>
                         {/* <Image style={{ width: width * 0.9, height: 200 }} borderRadius={15} source={{uri:'http://8.142.11.85:3000/public/images/zsb1.jpeg'}} /> */}
-                        <Text style={{ fontSize: 16, textAlign: "center", marginTop: 10, fontWeight: "bold", width: width * 0.9, color: "#333" }}>浙江省金融业发展“十四五”规划发布——未来金融 “数智”先行</Text>
+                        <Text style={{ fontSize: 16, textAlign: "center", marginTop: 10, fontWeight: "bold", width: width * 0.9, color: "#333" }}>浙江省出台计划推进文旅高质量发展</Text>
                         <View style={{ flexDirection: "row", marginTop: 5 }}>
                             <Text style={{ fontSize: 10, marginLeft: "-45%" }}>#浙江日报</Text>
                             <Text style={{ fontSize: 10 }}>|2021-7-13</Text>
                         </View>
                         <View style={{ borderStyle: "dashed", borderRadius: 5, borderWidth: 0.5, width: width * 0.9, alignItems: "center", marginBottom: 5, marginTop: 5 }}>
                             <Text style={{ fontSize: 13, marginTop: 5, lineHeight: 25, marginHorizontal: width * 0.025, color: "#333" }}>
-                                &emsp;&emsp;浙江省金融业发展轮廓愈发清晰。近日，浙江省政府发布《浙江省金融业发展“十四五”规划》（以下简称《规划》），提出“十四五”期间，要围绕服务新发展格局，基本建成高端资源集聚的金融服务战略支点、内外循环相互促进的金融要素配置枢纽，打造金融高质量发展强省和区域金融现代治理先行示范省。
+                                &emsp;&emsp;如今，家住杭州临安区的陈女士早已习惯了这样的文化生活——在杭州时去浙江图书馆借书，周末回家后将图书归还到临安图书馆。早在今年初，两家图书馆就实现了共享通借通还业务。不仅如此，在临安，文化馆、图书馆、博物馆样样俱全，连村里的文化礼堂都越来越有“文艺范儿”。未来5年里，陈女士还能在“15分钟品质文化生活圈”里享受到更丰富的公共文化资源。近日，省文化和旅游厅印发了《浙江省文化和旅游厅推进文化和旅游高质量发展促进共同富裕示范区建设行动计划（2021-2025年）》（下称《计划》）。《计划》提出，浙江将在“物质富裕、精神富有”上双向发力，到2025年基本建成新时代文化高地、中国最佳旅游目的地、全国文化和旅游融合发展样板地，为全国文化和旅游系统提供可复制、可推广的“浙江经验”。
                             </Text>
                         </View>
                         {/* <Image style={{ width: width * 0.9, height: 200, marginTop: 10, marginBottom: 10 }} borderRadius={15} source={{ uri: 'http://8.142.11.85:3000/public/images/zsb3.jpeg' }} />
@@ -41,19 +41,19 @@ export default class ZhenCe extends Component {
 
                         <View style={{ borderStyle: "dashed", borderRadius: 5, borderWidth: 0.5, width: width * 0.9, alignItems: "center", marginBottom: 5, marginTop: 5 }}>
                             <Text style={{ fontSize: 13, marginTop: 5, lineHeight: 25, marginHorizontal: width * 0.025, color: "#333" }}>
-                                &emsp;&emsp;“此次规划的主要目标，可以简要概括为‘1个体系+1234’。”省地方金融监管局负责人告诉记者。一个体系，指数智化区域金融运行体系，主要是“搭建一个平台，提升四大能力”。在今年全省金融工作座谈会上，省委已提出明确要求——搭建富有浙江特色的数智金融平台，提升服务实体经济能力、服务百姓普惠金融能力、金融产业高质量发展能力和金融风险防控处置能力。“1234”，即加快打造全国一流新兴金融中心，深入实施融资畅通工程、“凤凰行动”两个升级版，积极打造国际金融科技创新、多层次资本市场发展、民营和中小微企业金融服务三大高地，联动建设具有全国引领示范效应的科创金融、绿色金融、普惠金融和开放金融四大特色带。
+                                &emsp;&emsp;为推动公共文化服务现代化先行省的建成，《计划》提出了公共文化服务标准2.0版。首先在硬件设施上升级：每个市要有图书馆、文化馆、博物馆、非遗馆和美术馆，实现“市有五馆一院一厅，县有四馆一院，区有三馆，乡镇（街道）有综合文化站，村有农村文化礼堂”的全覆盖。文化设施建成后，如何发挥作用？省文化和旅游厅相关负责人告诉记者，在公共文化服务领域可以引入社会力量，如萧山的“文化管家”运营管理模式，让专业团队来代管、托管场所，为老百姓提供真正需要的服务。
                             </Text>
                         </View>
                         <View><Text style={{ fontSize: 15, fontWeight: "bold", marginTop: 10, color: "#333" }}>牵住“数智金融”牛鼻子</Text></View>
                         <View style={{ borderStyle: "dashed", borderRadius: 5, borderWidth: 0.5, width: width * 0.9, alignItems: "center", marginTop: 5, marginBottom: 5 }}>
                             <Text style={{ fontSize: 13, marginTop: 5, lineHeight: 25, marginHorizontal: width * 0.025, color: "#333" }}>
-                                &emsp;&emsp;《规划》提出加快打造全国一流新兴金融中心。“新”在哪？记者注意到，《规划》中，“数智”一词出现了10次。浙江大学公共政策研究院执行院长金雪军认为，《规划》指出了方向，“要牵住‘数智金融’的牛鼻子。在长三角一体化背景下，浙江要体现比较优势，实现分工协作、错位发展。”“构建数智化区域金融运行体系，打造数智金融先行省”，正是下一个五年我省金融发展的主线。作为全国数字经济先行省份，浙江的数字生态展现出勃勃生机。如何继续深化这一重要特色，是把握下一阶段金融业发展的关键。省地方金融监管局负责人表示，构建数智化区域金融运行体系，核心是搭建富有浙江特色的数智金融平台，探索构建“金融大脑”，形成“平台+大脑+场景应用”的体制。这就要求打通集成各类金融服务平台，横向进一步协同各类金融服务平台，纵向加快省市县三级平台贯通。
+                                &emsp;&emsp;《计划》还关注基层文艺队伍建设，要求推进乡村文艺团队“三团三社”（合唱团、民乐团、艺术团、文学社、摄影社、书画社）建设，到“十四五”末，“三团三社”数量从如今的3万余支增加到4万支。同时，开展文化示范户和乡村文化能人培育，提升乡村文化的造血功能。《计划》提出，“十四五”期间，浙江将实施促进居民收入十年倍增计划，努力成为旅游富民的省域范例。到2025年，旅游业对国民经济的综合贡献率达19%，农村居民的旅游收入占其可支配收入的比重达13%以上。
                             </Text>
                         </View>
                         <Image style={{ width: width * 0.9, height: 200 }} borderRadius={15} source={{ uri: 'http://8.142.11.85:3000/public/images/zsb2.jpeg' }} />
                         <View style={{ borderStyle: "dashed", borderRadius: 5, borderRadius: 5, borderWidth: 0.5, width: width * 0.9, alignItems: "center", marginBottom: 5, marginTop: 10 }}>
                             <Text style={{ fontSize: 13, marginTop: 5, lineHeight: 25, marginHorizontal: width * 0.025, color: "#333" }}>
-                                &emsp;&emsp;“‘金融大脑’则像一个中枢系统，能实现政府、金融机构、企业、居民等各方面数据的集成共享。”金雪军认为。大数据、云计算、区块链、人工智能等数字技术奠定了体系构建的基石，须同步发展，合力形成完整的金融技术生态系统。最终，不断完善运用场景，将金融与产业链、供应链等实体经济体系深度融合，形成完善的“链网式金融”。建设数智化区域金融运行体系，金雪军有几个提醒，“首先是把握金融体系建设不是孤立的，而是要与供应链、产业链协调对接，加强技术和场景的深度融合，形成完整系统。同时，积极争取数字人民币试点，这对金融大脑构建具有重要推动意义。此外，风控和监管要跟上。”
+                                &emsp;&emsp;省文化和旅游厅提出，不仅要协调城乡均衡，还要促进区域协调发展，目标将地区人均文化和旅游总收入最高最低倍差缩小到2.1以下。为此，省文化和旅游厅等6部门此前印发了《关于加快推动山区26县旅游业高质量发展的意见》，提出到2025年，实现山区26县旅游总产出、游客总人次年均分别增长5%以上。
                             </Text>
                         </View>
                         <View><Text style={{ fontSize: 15, fontWeight: "bold", marginTop: 10, color: "#333" }}>统筹区域金融发展</Text></View>
@@ -64,10 +64,10 @@ export default class ZhenCe extends Component {
                         </View>
                         <View style={{ borderStyle: "dashed", borderRadius: 5, borderWidth: 0.5, width: width * 0.9, alignItems: "center", marginBottom: 5, marginTop: 5 }}>
                             <Text style={{ fontSize: 13, marginTop: 5, lineHeight: 25, marginHorizontal: width * 0.025, color: "#333" }}>
-                                &emsp;&emsp;围绕加快打造全国一流新兴金融中心这个任务，各地可在区域金融改革的基础上充分发挥各自的比较优势，从而复制推广一批成熟有效的金融改革经验。推动金融促进高质量发展建设共同富裕示范区是统筹区域金融发展的应有之义。《规划》提出，实施金融帮扶山区26县工作举措，做好政策性、开发性金融机构“共同富裕”等专项融资对接，推动跨越式发展。下一步，我省还将出台金融支持高质量发展建设共同富裕示范区的政策措施。将目光望向更远处，浙江金融业发展也应为推动长三角高质量一体化发展提供有力保障。“这要求我们主动对接上海国际金融中心建设，进一步协同完善长三角金融一体化合作机制，如加强地方金融立法、区域金融改革等方面合作，推进金融联合监管、风险联防联控等机制建设。同时，扩大区域金融高水平双向开放，推进跨境资金流动便利化，提高金融业国际化发展水平。”省地方金融监管局负责人表示。
+                                &emsp;&emsp;在提升旅游品质的同时，还要丰富旅游业态。浙江争取把舟山、宁波等市列入中国邮轮旅游发展实验区，协调国际、国内邮轮线路延伸至浙江十大海岛公园和滨海景区景点，将邮轮游与海岛游相结合，积极探索海上绿水青山就是金山银山理念旅游转化路径。
                             </Text>
                         </View>
-                        <View><Text style={{ fontSize: 15, fontWeight: "bold", marginTop: 10, color: "#333" }}>打造“凤凰行动”升级版</Text></View>
+                        {/* <View><Text style={{ fontSize: 15, fontWeight: "bold", marginTop: 10, color: "#333" }}>打造“凤凰行动”升级版</Text></View>
                         <View style={{ borderStyle: "dashed", borderRadius: 5, borderWidth: 0.5, width: width * 0.9, alignItems: "center", marginBottom: 5, marginTop: 5 }}>
                             <Text style={{ fontSize: 13, marginTop: 5, lineHeight: 25, marginHorizontal: width * 0.025, color: "#333" }}>
                                 &emsp;&emsp;《规划》提出，全力打造“凤凰行动”升级版。“凤凰行动”启动以来，浙江资本市场实现飞跃。“十三五”期间，浙江新增境内上市公司219家，累计达518家，均保持全国第二。如果算上境外上市公司，则累计达到659家。省地方金融监管局负责人表示，“十四五”要通过资金全过程参与、政策全周期支持和服务全链条保障，力争境内外上市公司达1000家，每年动态保有股份制企业1万家以上、重点上市后备企业1000家以上和报会（交易所）企业100家以上，“打造一批以上市公司为龙头的现代产业集群，促进浙江经济高质量发展。”
@@ -77,7 +77,7 @@ export default class ZhenCe extends Component {
                             <Text style={{ fontSize: 13, marginTop: 5, lineHeight: 25, marginHorizontal: width * 0.025, color: "#333" }}>
                                 &emsp;&emsp;浙江企业将迎来哪些利好？金雪军认为，“全链条”建设是“凤凰行动”升级版的最大特色。“这不光是一个简单的上市工作，还包括股改、再融资、并购等环节，涵盖企业培育到上市之后的全过程。”升级版的“凤凰行动”将更注重“全方位、全要素、全市场、全系统”。除了企业自身推进外，在这个过程中，还需要在企业发展的不同阶段丰富金融机构参与场景，各类金融资源共同发力，不仅助力企业做大做强，也反向推动金融机构自身的发展，实现双赢。而多层次资本市场体系也将得到完善。其中，大力推动上市公司引领高质量发展仍是“凤凰行动”升级版的核心。浙江企业通过与资本市场对接，进一步加强科技创新，提升企业治理水平，最终实现产业引领。
                             </Text>
-                        </View>
+                        </View> */}
                         {/* <Image style={{ width: width * 0.9, height: 200, marginTop: 10, marginBottom: 10 }} borderRadius={15} source={{ uri: 'http://8.142.11.85:3000/public/images/zsb4.jpeg' }} /> */}
                     </View>
                 </ScrollView>
