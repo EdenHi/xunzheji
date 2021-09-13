d7e9d2e588e72d2e4197b965241f7b6e99fba10c
ppx <572504596@qq.com>
2021-09-13 19:25:22 +0800

    1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import './src/components/comment/back'
 import {name as appName} from './app.json';
-import   changeSVGColor from './src/components/ChangeLottie/index'
+//import   changeSVGColor from './src/components/ChangeLottie/index'
 LogBox.ignoreAllLogs(true); 
 
 AppRegistry.registerComponent(appName, () => App);
