26e86ea098e15d39041352fe40abdfec413f6aa6
Git1 <2482394937@qq.com>
2021-09-22 09:28:54 +0800

    123

--- src/pages/Road/Video.js
+++ src/pages/Road/Video.js
@@ -33,6 +33,7 @@ export default class componentName extends Component {
             mission3: false,
             shoturi: 'http://47.100.78.254:3000/public/images/initimg.jpg',
             jinbi:'',
+            kilo:0,
         }
     }
 
@@ -397,8 +398,8 @@ export default class componentName extends Component {
                         <TouchableOpacity activeOpacity={1} onPress={() => { this.setModalVisible4(!modalVisible4) }} style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.4)' }}>
                             <View collapsable={false} ref={viewRef} style={{ borderWidth: 1, height: height * 0.83 }}>
                                 <ImageBackground resizeMode={'stretch'} style={{ flex: 5, width: '80%', height: height * 0.6, marginLeft: '10%', marginTop: height * 0.15, borderWidth: 0, }} imageStyle={{ width: '100%' }} borderRadius={10} source={require('../img/haibao.png')}>
-                                    <View style={{ height: height * 0.05, width: '90%', marginLeft: '4.7%', borderColor: 'red', marginTop: height * 0.433, borderWidth: 0 }}>
-                                        <Text style={{ color: '#fff', fontSize: 20, fontWeight: 'bold', textAlign: 'center', width: '100%', height: '100%', textAlignVertical: 'center' }}>已抵达孝顺镇{ }，累计走过{ }00公里</Text>
+                                    <View style={{ height: height * 0.08, width: '90%', marginLeft: '4.7%', borderColor: 'red', marginTop: height * 0.42, borderWidth: 0 }}>
+                                        <Text style={{marginTop:height*-0.175,color: '#fff', fontSize: 20, fontWeight: 'bold', textAlign: 'center', width: '90%',alignSelf:'center', height: '100%', textAlignVertical: 'center' }}>大约走过{this.state.step}Km，祝你快乐生活每一天</Text>
                                     </View>
                                 </ImageBackground>
                             </View>
